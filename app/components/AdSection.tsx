676e53a9a806252e83be144576a2af9521317815
Ratidzo <ratygore@gmail.com>
2026-03-12 18:05:47 +0200

    dev: begin the video wars

--- app/components/AdSection.tsx
+++ app/components/AdSection.tsx
@@ -1,8 +1,10 @@
 
+import Ad from "./media/Ad"
+
 export default function AdSection() {
    return (
-    <div className="h-100 bg-sky w-full">
-
+    <div className="bg-sky w-full p-6 h-160 flex justify-center items-center ">
+        <Ad />
     </div>
    ) 
 }
--- app/components/media/Ad.tsx
+++ app/components/media/Ad.tsx
@@ -4,5 +4,13 @@ import tryit from '../../../videos/tryit.mp4';
 
 export default function Ad() {
 
-    return <Video src={tryit} />
+    return <Video src={tryit} style={{
+        maxWidth: "1000px",
+        borderRadius: "4px"
+    }} 
+        autoPlay 
+        loop 
+        muted 
+        controls={false}
+        className="h-full w-full rounded-sm" />
 }
