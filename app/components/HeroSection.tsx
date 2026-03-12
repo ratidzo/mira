9d4de60dac53c169b8b2cb6b7fbd10951a12a105
Ratidzo <ratygore@gmail.com>
2026-03-12 13:11:33 +0200

    dev: setup glass panel effect for hero bg

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -12,11 +12,22 @@ export default function HeroSection() {
     const panelOverlap = 16; // layered glass effect.
     const panelCount = Math.floor(currentContainerWidth / (panelWidth - panelOverlap));
 
+    useEffect(() => {
+        if (!containerRef.current) return;
+        const observer = new ResizeObserver((entries) => {
+            for (const entry of entries) {
+                setCurrentContainerWidth(entry.contentRect.width)
+            }
+        });
+        observer.observe(containerRef.current);
 
+        // Cleanup
+        return () => observer.disconnect();
+    }, []);
 
 
     return (
-        <section className="relative flex w-full h-full justify-center items-center overflow-x-hidden bg-white select-none">
+        <section ref={containerRef} className="relative flex w-full h-full justify-center items-center overflow-x-hidden bg-white select-none">
             <Image width={260} height={200} src={"/Left.png"} alt="left image" className="absolute z-0 left-[10%] top-[10%]" />
             <Image width={229} height={420} src={"/Middle.png"} alt="middle image" className="absolute z-0 left-[40%] top-[55%]" />
             <Image width={229} height={420} src={"/Right.png"} alt="right image" className="absolute z-0 right-[20%] top-[10%]" />
@@ -25,6 +36,19 @@ export default function HeroSection() {
             <Image width={120} height={120} src={"/Skyblue Blob.svg"} alt="left blob" className="absolute z-0 left-[30%] top-[20%]" />
             <Image width={120} height={120} src={"/Black Blob.svg"} alt="middle blob" className="absolute z-0 left-[55%] top-[70%]" />
             <Image width={120} height={120} src={"/Pink Blob.svg"} alt="right blob" className="absolute z-0 right-[10%] top-[20%]" />
+
+            { Array.from({length: panelCount}).map((_, index) => (
+                <div key={index}
+                style={{
+                    position: "absolute",
+                    left: `${index * (panelWidth - panelOverlap)}px`,
+                    width: `${panelWidth}px`,
+                    zIndex: index
+                }}
+                className="h-full bg-linear-to-b from-white/0 to-gray-200/25 backdrop-blur-sm" />
+            )) }
+
+
 
         </section>
     )
