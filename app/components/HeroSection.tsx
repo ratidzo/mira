c836488dac0834ed8a3f0561f6d4ad3c5c10f0f0
Ratidzo <ratygore@gmail.com>
2026-03-12 08:13:01 +0200

    dev: setup project folder structure

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function HeroSection() {
+
+    return (
+        <section>
+            <h1>This is the hero section.</h1>
+
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
 
     return (
-        <div className="flex flex-col gap-8 justify-center items-center w-full h-full">
-            <h1 className="font-app text-5xl text-gray-700">Hello in Inter Font</h1>
-            <h1 className="font-brand text-5xl text-pink-400">Let them look in the mirror</h1>
+        <div>
+
         </div>
     )
 }
