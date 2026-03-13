f678cf6349eaef01f33cccdbacbb91865607c589
Ratidzo <ratygore@gmail.com>
2026-03-13 10:43:06 +0200

    dev: wireup the logos to the ticker

--- app/components/BrandsSection.tsx
+++ app/components/BrandsSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import zara from "@/public/zara.svg";
 import lvhm from "@/public/louis vuitton.svg"
 import skims from "@/public/skims.svg"
@@ -5,12 +7,22 @@ import alo from "@/public/alo.svg"
 import gymshark from "@/public/gymshark.svg"
 import chanel from "@/public/chanel.svg"
 
+import Image from "next/image";
+import Ticker from "./UI/Ticker";
+
 
 export default function BrandsSection() {
 
     return (
        <div className="bg-dark-900 w-full h-100">
-
+        <Ticker gap={120}>
+            <Image src={lvhm} alt="Louis Vuitton logo" height={24} width={230}/>
+            <Image src={gymshark} alt="Gymshark logo" height={24} width={144} />
+            <Image src={skims} alt="Skims logo" height={28} width={126} />
+            <Image src={chanel} alt="Chanel logo" height={24} width={152} />
+            <Image src={alo} alt="Alo logo" height={40} width={59} />
+            <Image src={zara} alt="Zara logo" height={24} width={102} />
+        </Ticker> 
        </div> 
     )
 }
