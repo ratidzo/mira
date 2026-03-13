fc93a52fa20abdf0aa6a9cb09788aa362d03555c
Ratidzo <ratygore@gmail.com>
2026-03-13 21:39:19 +0200

    dev: define hover on interaction

--- app/components/BrandsSection.tsx
+++ app/components/BrandsSection.tsx
@@ -14,9 +14,9 @@ import Ticker from "./UI/Ticker";
 export default function BrandsSection() {
 
     return (
-       <div className="bg-dark-900 w-full flex flex-col justify-center items-center pt-16  pb-40 gap-20">
+       <div className="bg-dark-900 w-full flex flex-col justify-center items-center pt-16  pb-20 gap-10">
         <h1 className="font-brand text-lg text-white">Trusted by the best in e-commerce:</h1>
-        <Ticker gap={80} speed={40}>
+        <Ticker gap={80} speed={40} pauseOnHover>
             <Image src={lvhm} alt="Louis Vuitton logo" height={24} width={230}/>
             <Image src={gymshark} alt="Gymshark logo" height={24} width={144} />
             <Image src={skims} alt="Skims logo" height={28} width={126} />
--- app/components/UI/Ticker.tsx
+++ app/components/UI/Ticker.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Children, ReactNode } from "react";
+import { Children, ReactNode, useState } from "react";
 
 interface TickerProps {
     children: ReactNode;
@@ -14,9 +14,11 @@ export default function Ticker({
     children,
     speed = 20,
     direction = "left",
+    pauseOnHover = false,
     gap = 64,
 }: TickerProps) {
 
+    const [isPaused, setIsPaused] = useState(false);
     const animationName = direction === "left" ? "ticker-left": "ticker-right";
 
     const items = Children.map(children, (child, index) => (
@@ -26,22 +28,41 @@ export default function Ticker({
         </li>
     ))
 
+    const handleMouseEnter = () => {
+
+        console.log("Mouse enter ")
+
+        if(pauseOnHover) {
+            setIsPaused(true);
+        }
+    }
+
+    const handleMouseLeave = () => {
+        console.log("Mouse leave")
+
+        if(pauseOnHover) {
+            setIsPaused(false);
+        }
+    }
+
     return (
         <div
-            className="w-full overflow-hidden"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="w-full overflow-hidden py-10 hover:cursor-pointer"
             style={{
                 maskImage: "linear-gradient(to right, transparent, black 15%, black 85%, transparent)",
                 WebkitMaskImage: "linear-gradient(to right, transparent, black 15%, black 85%, transparent)"
             }}
             >
                 <div 
-                    className="w-max flex justify-center items-center gap-0"
+                    className="w-max flex justify-center items-center gap-0 "
                     style={{
                         animationName,
                         animationDuration: `${speed}s`,
                         animationTimingFunction: "linear",
                         animationIterationCount: "infinite",
-                        animationPlayState: "running",
+                        animationPlayState: `${isPaused ? "paused": "running"}` 
                     }}>
                        <ul className="flex w-full items-center"
                         style={{ gap: `${gap}px`}}
