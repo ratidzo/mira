fa70d5f887c9fe5ed58225b887c689b671745dc9
Ratidzo <ratygore@gmail.com>
2026-03-16 16:19:06 +0200

    dev: setup nav links.

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,11 +4,41 @@ import Button from "./UI/Button.tsx";
 import { useState } from "react";
 
 
+const links = [
+    {
+        id: 1,
+        title: "Pricing",
+        url: "#"
+    },
+    {
+        id: 2,
+        title: "Product",
+        url: "#"
+    },
+    {
+        id: 3,
+        title: "Company",
+        url: "#"
+    }
+]
+
 export default function Nav() {
 
     return (
         <nav>
-            <p>This is a nav section.</p>
+            <div>
+                <h1> Logo </h1>
+            </div>
+
+            <ul className="flex gap-4 items-center z-100">
+            {
+                links.map((link) => (
+                    <li key={link.id}> { link.title } </li>
+                ))
+            } 
+            </ul>
+
+            <Button text="Demo" />
         </nav>
     )
 }
