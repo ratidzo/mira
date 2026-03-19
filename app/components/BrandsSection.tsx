ced52fee1cba5dc84cb8532b132b76611e2b8aa8
Ratidzo <ratygore@gmail.com>
2026-03-19 16:15:12 +0200

    refactor: move logo items to a list and pass it in the children prop of the Ticker component.

--- app/components/BrandsSection.tsx
+++ app/components/BrandsSection.tsx
@@ -13,17 +13,19 @@ import Ticker from "./UI/Ticker";
 
 export default function BrandsSection() {
 
+    const logos = [
+            <Image src={lvhm} alt="Louis Vuitton logo" height={24} width={230}/>,
+            <Image src={gymshark} alt="Gymshark logo" height={24} width={144} />,
+            <Image src={skims} alt="Skims logo" height={28} width={126} />,
+            <Image src={chanel} alt="Chanel logo" height={24} width={152} />,
+            <Image src={alo} alt="Alo logo" height={40} width={59} />,
+            <Image src={zara} alt="Zara logo" height={24} width={102} />,
+    ]
+
     return (
        <div className="bg-dark-900 w-full flex flex-col justify-center items-center pt-16  pb-20 gap-10">
         <h1 className="font-brand text-lg text-white">Trusted by the best in e-commerce:</h1>
-        <Ticker gap={80} speed={20} direction="left" hoverInteractions hoverTimeStretch={0.3}>
-            <Image src={lvhm} alt="Louis Vuitton logo" height={24} width={230}/>
-            <Image src={gymshark} alt="Gymshark logo" height={24} width={144} />
-            <Image src={skims} alt="Skims logo" height={28} width={126} />
-            <Image src={chanel} alt="Chanel logo" height={24} width={152} />
-            <Image src={alo} alt="Alo logo" height={40} width={59} />
-            <Image src={zara} alt="Zara logo" height={24} width={102} />
-        </Ticker> 
-       </div> 
+        <Ticker children={logos} gap={80} speed={30} direction="left" hoverInteractions hoverTimeStretch={0.5} />
+       </div>
     )
 }
