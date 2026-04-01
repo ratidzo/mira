a6284199252f10e7efa6be48a3b3e27e453fbd48
Ratidzo <ratygore@gmail.com>
2026-04-01 14:27:33 +0200

    Nav: setup nav layout and background style

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -25,18 +25,20 @@ const links = [
 export default function Nav() {
 
     return (
-        <nav className="flex">
+        <nav className="flex items-center gap-40 border broder-2 px-10 py-2 bg-white/75 backdrop-blur-lg sticky top-0 z-200">
+        <div className="flex gap-20">
             <div>
                 <h1> Logo </h1>
             </div>
 
-            <ul className="flex gap-4 items-center z-100">
+            <ul className="flex gap-12 items-center z-100">
             {
                 links.map((link) => (
                     <li key={link.id}> { link.title } </li>
                 ))
             } 
             </ul>
+        </div>
 
             <Button text="Demo" />
         </nav>
