90941ef02e37808250d669e57dcd50585251f07c
Ratidzo <ratygore@gmail.com>
2026-04-01 19:14:49 +0200

    Ad: use native html video element

--- app/components/AdSection.tsx
+++ app/components/AdSection.tsx
@@ -3,7 +3,7 @@ import Ad from "./media/Ad"
 
 export default function AdSection() {
    return (
-    <div className="bg-sky w-full p-6 h-160 flex justify-center items-center ">
+    <div className="bg-sky w-full p-8 h-160 flex justify-center items-center ">
         <Ad />
     </div>
    ) 
--- app/components/media/Ad.tsx
+++ app/components/media/Ad.tsx
@@ -1,16 +1,11 @@
-import Video from "next-video";
 import tryit from '../../../videos/tryit.mp4';
 
 
 export default function Ad() {
 
-    return <Video src={tryit} style={{
-        maxWidth: "1000px",
-        borderRadius: "4px"
-    }} 
-        autoPlay 
-        loop 
-        muted 
-        controls={false}
-        className="h-full w-full rounded-sm" />
+    return (
+        <video className="w-[60vw]"  loop autoPlay muted>
+            <source src="tryit.mp4" type="video/mp4" />
+        </video>
+    )
 }
