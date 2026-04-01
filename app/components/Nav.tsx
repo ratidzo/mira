fd281e256848d323084a93c74d33da15abf05a4f
Ratidzo <ratygore@gmail.com>
2026-04-01 14:37:27 +0200

    Nav: setup clickable svg logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Button from "./UI/Button";
+import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 
@@ -25,11 +27,14 @@ const links = [
 export default function Nav() {
 
     return (
-        <nav className="flex items-center gap-40 border broder-2 px-10 py-2 bg-white/75 backdrop-blur-lg sticky top-0 z-200">
-        <div className="flex gap-20">
-            <div>
-                <h1> Logo </h1>
+        <nav className="flex items-center gap-60 border broder-2 px-10 py-2 bg-white/75 backdrop-blur-lg sticky top-0 z-200">
+        <div className="flex gap-16">
+            <Link href="/">
+            <div className="flex gap-2 items-center">
+                <Image alt="Mira logo" src="mira.svg" width="26" height="29" />
+                <span className="font-brand">Mira</span>
             </div>
+            </Link>
 
             <ul className="flex gap-12 items-center z-100">
             {
