fb667e340d49fcce459f1ba48cb45b452c5194e5
Ratidzo <ratygore@gmail.com>
2026-04-10 15:02:58 +0200

    nav: make nav sticky the right way

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -27,8 +27,7 @@ const links = [
 export default function Nav() {
 
     return (
-        <div className="p-[2px] bg-linear-to-r from-rose/50 to-dark-200/75 z-200 sticky top-0">
-        <nav className="flex items-center gap-60 px-10 py-2 bg-white/75 backdrop-blur-lg z-200">
+        <nav className="flex items-center sticky top-10 gap-60 px-10 py-2 bg-white/75 backdrop-blur-lg z-200 border" >
         <div className="flex gap-16">
             <Link href="/">
             <div className="flex gap-2 items-center">
@@ -48,6 +47,5 @@ export default function Nav() {
 
             <Button text="Demo" />
         </nav>
-        </div>
     )
 }
